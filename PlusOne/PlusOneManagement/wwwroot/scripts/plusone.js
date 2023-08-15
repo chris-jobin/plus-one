c263adf4a23db319853b24f2f968f828f6db520d
Chris Jobin <c.r.jobin@gmail.com>
2023-08-15 17:36:43 -0500

    Added basic modal component

---
+++ PlusOne/PlusOneManagement/wwwroot/scripts/plusone.js
@@ -0,0 +1,12 @@
+﻿
+function modalOpen(id) {
+    let modal = document.getElementById(id);
+    modal.style.display = "flex";
+    modal.showModal();
+}
+
+function modalClose(id) {
+    let modal = document.getElementById(id);
+    modal.style.display = "";
+    modal.close();
+}
